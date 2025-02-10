39251fd8839ad57ef52a70038035536f0db88f53
Laicurè Leviarè <Laicure@users.noreply.github.com>
2025-02-11 01:54:18 +0800

    updated chatgpted Mod_SQLite.vb

--- Mod_SQLite.vb
+++ Mod_SQLite.vb
@@ -1,4 +1,5 @@
 Imports System.Data.SQLite
+Imports System.Text
 
 Module Mod_SQLite
 
@@ -6,54 +7,47 @@ Module Mod_SQLite
     Friend str_SQLiteConn As String = ""
 
     Friend Function SQLReadQuery(comText As String, Optional Paramzz As List(Of SQLParamz) = Nothing) As DataTable
+        Dim dtx As New DataTable()
         If Paramzz Is Nothing Then Paramzz = New List(Of SQLParamz)()
 
-        Using conX As New SQLiteConnection(str_SQLiteConn),
-              comX As New SQLiteCommand(comText.Trim(), conX),
-              dtx As New DataTable()
+        Using conX As New SQLiteConnection(str_SQLiteConn), comX As New SQLiteCommand(comText.Trim(), conX)
+            conX.Open()
 
-            If conX.State = ConnectionState.Closed Then conX.Open()
-
-            ' Add parameters if present
+            ' Add parameters
             For Each param In Paramzz
-                If comText.Contains(param.Parame) Then
-                    comX.Parameters.AddWithValue(param.Parame, param.Value)
-                End If
+                comX.Parameters.AddWithValue(param.Parame, param.Value)
             Next
 
+            ' Execute query and load data
             Using readerX As SQLiteDataReader = comX.ExecuteReader()
                 dtx.Load(readerX)
             End Using
-
-            Return dtx
         End Using
+
+        Return dtx
     End Function
 
     Friend Sub SQLWriteQuery(comText As String, Optional Paramzz As List(Of SQLParamz) = Nothing, Optional useTransaction As Boolean = True)
         If Paramzz Is Nothing Then Paramzz = New List(Of SQLParamz)()
 
-        Using conX As New SQLiteConnection(str_SQLiteConn),
-              comX As New SQLiteCommand(comText.Trim(), conX)
-            
-            If conX.State = ConnectionState.Closed Then conX.Open()
+        Using conX As New SQLiteConnection(str_SQLiteConn), comX As New SQLiteCommand(comText.Trim(), conX)
+            conX.Open()
 
-            ' Add parameters if present
+            ' Add parameters
             For Each param In Paramzz
-                If comText.Contains(param.Parame) Then
-                    comX.Parameters.AddWithValue(param.Parame, param.Value)
-                End If
+                comX.Parameters.AddWithValue(param.Parame, param.Value)
             Next
 
             If useTransaction Then
                 Using transX As SQLiteTransaction = conX.BeginTransaction()
+                    comX.Transaction = transX
                     Try
-                        comX.Transaction = transX
                         comX.ExecuteNonQuery()
                         transX.Commit()
                         Console.WriteLine("SQLWriteQuery: Written!")
                     Catch ex As Exception
                         transX.Rollback()
-                        Console.WriteLine($"SQLWriteQuery Error: {ex.Message}")
+                        Console.WriteLine("SQLWriteQuery Error: " & ex.Message)
                         Throw
                     End Try
                 End Using
@@ -64,47 +58,49 @@ Module Mod_SQLite
     End Sub
 
     Friend Sub SQLDtToDb(DtSource As DataTable, SQLTable As String, SQLColumns() As String)
-        Using conX As New SQLiteConnection(str_SQLiteConn),
-              comX As New SQLiteCommand("", conX)
-            
-            If conX.State = ConnectionState.Closed Then conX.Open()
+        Using conX As New SQLiteConnection(str_SQLiteConn), comX As New SQLiteCommand("", conX)
+            conX.Open()
             Using transX As SQLiteTransaction = conX.BeginTransaction()
+                comX.Transaction = transX
                 Try
-                    comX.Transaction = transX
-                    comX.CommandText = $"DELETE FROM [{SQLTable}];"
+                    comX.CommandText = $"DELETE FROM {SQLTable};"
                     comX.ExecuteNonQuery()
 
                     If DtSource.Rows.Count > 0 Then
+                        Dim sb As New StringBuilder()
                         Dim insertCount As Integer = 0
-                        Dim transacQuery As New Text.StringBuilder()
 
                         For Each dr As DataRow In DtSource.Rows
                             insertCount += 1
-                            Dim valuez = DtSource.Columns.Cast(Of DataColumn)()
-                                        .Select(Function(dc) dr(dc).ToString().Replace("'", "''"))
-                                        .ToArray()
+                            Dim values As List(Of String) = New List(Of String)
 
-                            transacQuery.AppendLine($"INSERT INTO [{SQLTable}] ([{String.Join("], [", SQLColumns)}]) VALUES ('{String.Join("', '", valuez)}');")
+                            For Each dc As DataColumn In DtSource.Columns
+                                values.Add("'" & dr.Item(dc).ToString().Replace("'", "''") & "'")
+                            Next
 
+                            sb.AppendLine($"INSERT INTO [{SQLTable}] ([{String.Join("], [", SQLColumns)}]) VALUES ({String.Join(", ", values)});")
+
+                            ' Execute in batches
                             If insertCount = 777 Then
-                                comX.CommandText = transacQuery.ToString()
+                                comX.CommandText = sb.ToString()
                                 comX.ExecuteNonQuery()
-                                transacQuery.Clear()
+                                sb.Clear()
                                 insertCount = 0
                             End If
                         Next
 
-                        If transacQuery.Length > 0 Then
-                            comX.CommandText = transacQuery.ToString()
+                        ' Execute remaining queries
+                        If sb.Length > 0 Then
+                            comX.CommandText = sb.ToString()
                             comX.ExecuteNonQuery()
                         End If
                     End If
 
                     transX.Commit()
-                    Console.WriteLine($"{SQLTable}: Written @ {DateTime.UtcNow.Subtract(dtm_ExecStart).ToString().Substring(0, 8)}")
+                    Console.WriteLine($"{SQLTable}: Written @ {DateTime.UtcNow:HH:mm:ss}")
                 Catch ex As Exception
                     transX.Rollback()
-                    Console.WriteLine($"{SQLTable}: ({ex.Source}) {ex.Message}")
+                    Console.WriteLine($"{SQLTable} Error: {ex.Message}")
                     Throw
                 End Try
             End Using
@@ -112,3 +108,8 @@ Module Mod_SQLite
     End Sub
 
 End Module
+
+Friend Class SQLParamz
+    Friend Property Parame As String
+    Friend Property Value As Object
+End Class
